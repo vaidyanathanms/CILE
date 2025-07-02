500281d709519cdbb9957e4b0f5e973048301bec
vaidyanathanms_cades <vm5@ornl.gov>
2025-07-02 16:20:53 -0400

    updated with Subil and Omar's recommendations for multicore runs

--- src_sh/run_md.sh
+++ src_sh/run_md.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
+# Optimized code from Subil/Omar -  OLCFHELP-22622 [cades-help] Unable to run Gromacs utilities (Check email: June-03-2025)
+# Instructions to build GROMACS are in this email
+# Date: July-02-2025
 
 #SBATCH -A chem
 #SBATCH -p burst
 #SBATCH -t 23:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 32
-#SBATCH --mem=2G
+#SBATCH -N 4                    # 4 nodes
+#SBATCH --ntasks-per-node=4     # 4 tasks/node
+#SBATCH -c 8                    # 8 cores/task
+#SBATCH --mem=0
 #SBATCH -J I5p0D0p0L100
-#SBATCH -o out.%J
-#SBATCH -e err.%J
+#SBATCH -o outdir/out.%J
+#SBATCH -e outdir/err.%J
 
-module purge
-module load gromacs/5.1.2
+# Load modules
+module reset
+module load intel mkl fftw hwloc cmake
 
+# Export gcc path and num_threads
+export LD_LIBRARY_PATH=/sw/cades-open/gcc/12.2.0/lib64:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
+# Initializing jobs
 cd $SLURM_SUBMIT_DIR
-
-echo "begin job.."
+echo "begin job @start time: ${date}"
 echo $PWD
 
-mkdir -p outdir
-mkdir -p trajfiles
+# Run command
+gmx='${HOME}/gromacs-2024.5/install/bin/gmx_mpi'
 
 #---------------------------------------------------------Generate initial run----------------------------------
 finit_inp=./enermin.tpr
 if ! test -f "$finit_inp"; then
 	echo "begin generating enermin.tpr.."
 	# generate enermin files
-	srun gmx grompp -f minim.mdp -c main.pdb -p topol.top -o enermin.tpr
+	srun ${gmx} grompp -f minim.mdp -c main.pdb -p topol.top -o enermin.tpr
 	
 fi
 wait
 
-#---------------------------------------------------------Minimization -DFLEXIBLE----------------------------------
-finit2_inp=./min_noflex.tpr
-if ! test -f "$finit2_inp"; then
+#---------------------------------------------------------Minimization -----------------------------------------
+fmin_inp=./nvt.tpr 
+if ! test -f "$fmin_inp"; then
 	
 	echo "begin running enermin.tpr.."
 	# run enermin.tpr
-	srun gmx mdrun -s enermin.tpr -cpo state_min.cpt -cpi state_min.cpt -cpt 2 -g md_min.log -o traj_min.trr -e ener_min.edr -c confout_min.gro  
+	srun ${gmx} mdrun -s enermin.tpr -cpo state_min.cpt -cpi state_min.cpt -cpt 2 -g md_min.log -o traj_min.trr -e ener_min.edr -c confout_min.gro  
 	wait
 
-	echo "begin generating min_noflex.tpr.."
-	# generate enermin files with no -DFLEXIBLE
-	srun gmx grompp -f min_noflex.mdp -c confout_min.gro -p topol.top -o min_noflex.tpr
+	echo "begin generating nvt.tpr.."
+	# generate nvt files
+	srun ${gmx} grompp -f nvt.mdp -c confout_min2.gro -p topol.top -o nvt.tpr 
 	wait
 
         cp md_min.log trajfiles/md_min.log
@@ -55,39 +62,18 @@ if ! test -f "$finit2_inp"; then
 fi
 wait
 
-#----------------------------------------------------------Minimization no FLEXIBLE--------------------------------------
-fmin_inp=./nvt.tpr 
-if ! test -f "$fmin_inp"; then
-
-	echo "begin running min_noflex.tpr.."
-	# run enermin.tpr
-	srun gmx mdrun -s min_noflex.tpr -cpo state_min2.cpt -cpi state_min2.cpt -cpt 2 -g md_min2.log -o traj_min2.trr -e ener_min2.edr -c confout_min2.gro -notunepme 
-	wait
-
-	echo "begin generating nvt.tpr.."
-	# generate nvt files
-	srun gmx grompp -f nvt.mdp -c confout_min2.gro -p topol.top -o nvt.tpr 
-	wait
-
-        cp md_min.log trajfiles/md_min2.log
-	cp traj_min.trr trajfiles/traj_min2.trr
-	cp ener_min.edr trajfiles/ener_min2.edr
-	cp confout_min.gro trajfiles/confout_min2.gro
-fi
-wait
-
 #----------------------------------------------------------NVT Equilibration--------------------------------------------
-fnvt_inp=./npt_berendsen.tpr
+fnvt_inp=./npt_crescale.tpr
 if ! test -f "$fnvt_inp"; then
 
 	echo "begin running nvt.tpr.."
 	# run nvt.tpr
-	srun gmx mdrun -s nvt.tpr -cpo state_nvt.cpt -cpi state_nvt.cpt -cpt 5 -g md_nvt.log -o traj_nvt.trr -e ener_nvt.edr -c confout_nvt.gro  -notunepme
+	srun ${gmx} mdrun -s nvt.tpr -cpo state_nvt.cpt -cpi state_nvt.cpt -cpt 5 -g md_nvt.log -o traj_nvt.trr -e ener_nvt.edr -c confout_nvt.gro  -notunepme
 	wait
 
-	echo "begin generating npt_berendsen.tpr.."
-	# generate npt_berendsen files
-	srun gmx grompp -f npt_berendsen.mdp -c confout_nvt.gro -p topol.top -o npt_berendsen.tpr
+	echo "begin generating npt_crescale.tpr.."
+	# generate npt_crescale files
+	srun ${gmx} grompp -f npt_crescale.mdp -c confout_nvt.gro -p topol.top -o npt_crescale.tpr
 	wait
 
 	cp md_nvt.log trajfiles/md_nvt.log
@@ -101,25 +87,25 @@ wait
 fnpt_inp=./npt_main.tpr
 if ! test -f "$fnpt_inp"; then
 
-	echo "begin running npt_berendsen.tpr.."
-	# run npt_berendsen.tpr
-	srun gmx mdrun -s npt_berendsen.tpr -cpo state_npt_berendsen.cpt -cpi state_npt_berendsen.cpt -cpt 5 -g md_npt_berendsen.log -o traj_npt_berendsen.trr -e ener_npt_berendsen.edr -c confout_npt_berendsen.gro  
+	echo "begin running npt_c_rescale.tpr.."
+	# run npt_c_rescale.tpr
+	srun ${gmx} mdrun -s npt_c_rescale.tpr -cpo state_npt_c_rescale.cpt -cpi state_npt_c_rescale.cpt -cpt 5 -g md_npt_c_rescale.log -o traj_npt_c_rescale.trr -e ener_npt_c_rescale.edr -c confout_npt_c_rescale.gro  
 	wait
 
 	echo "begin generating npt_main.tpr.."
 	# generate npt_main files
-	srun gmx grompp -f npt_main.mdp -c confout_npt_berendsen.gro -p topol.top -o npt_main.tpr 
+	srun ${gmx} grompp -f npt_main.mdp -c confout_npt_c_rescale.gro -p topol.top -o npt_main.tpr 
 	wait
 
-	cp md_npt_berendsen.log trajfiles/md_npt_berendsen.log
-	cp traj_npt_berendsen.trr trajfiles/traj_npt_berendsen.trr
-	cp ener_npt_berendsen.edr trajfiles/ener_npt_berendsen.edr
-	cp confout_npt_berendsen.gro trajfiles/confout_npt_berendsen.gro
+	cp md_npt_c_rescale.log trajfiles/md_npt_c_rescale.log
+	cp traj_npt_c_rescale.trr trajfiles/traj_npt_c_rescale.trr
+	cp ener_npt_c_rescale.edr trajfiles/ener_npt_c_rescale.edr
+	cp confout_npt_c_rescale.gro trajfiles/confout_npt_c_rescale.gro
 	wait
 
         echo "begin running npt_main.tpr.."
         # run npt_main.tpr
-        srun gmx mdrun -s npt_main.tpr -cpo state_npt_main.cpt -cpi state_npt_main.cpt -cpt 5 -g md_npt_main.log -o traj_npt_main.trr -e ener_npt_main.edr -c confout_npt_main.gro -notunepme       
+        srun ${gmx} mdrun -s npt_main.tpr -cpo state_npt_main.cpt -cpi state_npt_main.cpt -cpt 5 -g md_npt_main.log -o traj_npt_main.trr -e ener_npt_main.edr -c confout_npt_main.gro -notunepme       
 	wait
 
         cp md_npt_main.log trajfiles/md_npt_main.log
@@ -131,7 +117,7 @@ else
 
         echo "begin running npt_main.tpr.."
         # run npt_main.tpr
-        srun gmx mdrun -s npt_main.tpr -cpo state_npt_main.cpt -cpi state_npt_main.cpt -cpt 5 -g md_npt_main.log -o traj_npt_main.trr -e ener_npt_main.edr -c confout_npt_main.gro -notunepme 
+        srun ${gmx} mdrun -s npt_main.tpr -cpo state_npt_main.cpt -cpi state_npt_main.cpt -cpt 5 -g md_npt_main.log -o traj_npt_main.trr -e ener_npt_main.edr -c confout_npt_main.gro -notunepme 
 	wait
 
 
--- src_sh/run_preprocess.sh
+++ src_sh/run_preprocess.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
+# Optimized code from Subil/Omar -  OLCFHELP-22622 [cades-help] Unable to run Gromacs utilities (Check email: June-03-2025)
+# Instructions to build GROMACS are in this email
+# Date: July-02-2025
 
 #SBATCH -A chem
 #SBATCH -p burst
-#SBATCH -t 00:10:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 36
-#SBATCH --mem=2G
-#SBATCH -J pp_I5p0D0p0
+#SBATCH -t 23:00:00
+#SBATCH -N 1                    # 1 node
+#SBATCH --ntasks-per-node=1     # 1 task/node
+#SBATCH -c 1                    # 1 core/task
+#SBATCH --mem=0
+#SBATCH -J p_I5p0D0p0L100
 #SBATCH -o out.%J
 #SBATCH -e err.%J
 
-module purge
-module load gromacs/5.1.2
+# Load modules
+module reset
+module load intel mkl fftw hwloc cmake
 
+# Export gcc path and num_threads
+export LD_LIBRARY_PATH=/sw/cades-open/gcc/12.2.0/lib64:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
+# Initializing jobs
 cd $SLURM_SUBMIT_DIR
-
-echo "begin job.."
+echo "begin job @start time: ${date}"
 echo $PWD
 
 mkdir -p initdir
 mkdir -p outdir
+mkdir -p trajfiles
+
+# Run command
+gmx="${HOME}/gromacs-2024.5/install/bin/gmx_mpi "
 
 # generate packmol files
 ~/tools/packmol/packmol < make_mixture.inp
 wait
 
 # editconf box
-srun gmx editconf -f mixture_5p0_0p0.pdb -bt cubic -d 0.5 -o main.pdb
+srun ${gmx} editconf -f mixture.pdb -bt cubic -d 0.5 -o main.pdb
 wait
 
 # make tpr file
-srun gmx grompp -f minim.mdp -p topol.top -c main.pdb -o enermin.tpr
+srun ${gmx} grompp -f minim.mdp -p topol.top -c main.pdb -o enermin.tpr
 wait
 
 cp *.pdb initdir/
