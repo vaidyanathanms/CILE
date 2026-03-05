c18984f1a27f65a0a33f8757ea8e95c509e3f38d
vaidyanathanms_cades <vm5@ornl.gov>
2026-03-04 20:18:01 -0500

    added ana.py for rdf and added corresponding defs in extra_functions.py

--- src_py/ana.py
+++ src_py/ana.py
@@ -3,49 +3,103 @@
 # Import modules
 import os
 import sys
-import numpy
+import numpy as np
 import re
 import shutil
 import glob
 import math
 import subprocess
-#from make_gmx_inputs import * # function definitions
+import extra_functions as ef
 
+#------------------------------------------------------------------
 # Input data
-salt_name = 'LiFSI' # main salt
-IL_name = 'C2C1Im'  # ionic liquid
-dil_name = 'TTE'    # diluent
-N_Li = 100 # number of Li molecules
-r_IL_to_Li_arr = [2.0,5.0] # ratio of IL to Li
-r_dil_to_Li_arr = [[0.0,0.2,0.6,2.0],
-                   [0.0,0.5,1.5,5.0]] # ratio of dil to Li
+runall   = 0 # 0 - copy, 1 - copy & run
+res_dil  = ['DFB','TTE','BTF'] # residue names
+res_org_cat = 'c4c'
+res_org_an  = 'PF6'
+res_salt_cat = 'Li'
+res_salt_an  = 'PF6'
+rdfset_tuple = [("PF6","P"),
+                ("PF6","FP")] #(resname,type)
 
-#---------directory info---------------------------------------
-maindir    = os.getcwd() #src_py dir
-home_path  = '/home/vm5'
-scr_path   = '/lustre/or-scratch/cades-birthright/vm5'
-src_f90    = home_path + '/all_codes/files_cile/src_f90' #f90 dir
-src_lmp    = home_path + '/all_codes/files_cile/src_lmp' #lmp dir
-src_tcl    = home_path + '/all_codes/files_cile/src_tcl' #tcl dir
-scratchdir = scr_path  + '/cile_diluent' #head scratch dir
+n_li_salt_arr = np.array([100,100,100]) # number of lithium salt
+rat_il_salt  = 2.0 #keep float
+rat_dil_salt = 2.0
 
-#--------Check scratchdir------------------------------------
-if not os.path.isdir(scratchdir):
-    raise RuntimeError(scratchdir + ' not found')
+sh_ana_fyle = 'run_rdf_pyinp.sh' #shell script name
 
-scrdir = scratchdir + '/' + salt_name + str(N_Li) # scr dir
-#--------Main analysis---------------------------------------
-for k, r_IL2Li in enumerate(r_IL_to_Li_arr):
-    d, i = math.modf(r_IL2Li)
-    r_il2li_str = str(int(i)) + 'p' + str(d).split('.')[1]
-    for r_dil2Li in r_dil_to_Li_arr[k]:
-        d2, i2 = math.modf(r_dil2Li)
-        r_dil2Li_str = str(int(i2)) + 'p' + str(d2).split('.')[1]
-        workdir = scrdir + '_' + IL_name + r_il2li_str \
-                   + '_' + dil_name + r_dil2Li_str
-        
-        if not os.path.isdir(workdir):
-            raise RuntimeError(scratchdir + ' not found')
+if res_salt_an.casefold() == res_org_an.casefold():
+    res_list = [res_org_cat,res_org_an,res_salt_cat]
+    hetero_salt = 0
+else:
+    res_list = [res_org_cat,res_org_an,res_salt_cat,res_salt_an]
+    hetero_salt = 1
+#------------------------------------------------------------------
 
-        print(f'Analyzing {il_name}:Li = {r_IL2Li}, {dil_name}:Li = {r_dil2Li}')
+# Directory Paths
+home_dir  = os.environ["HOME"]
+main_dir  = os.getcwd() # current dir
+gmx_dir   = home_dir + '/all_codes/files_cile/src_gmx' # gmx file super directory
+sh_dir    = home_dir + '/all_codes/files_cile/src_sh'  # sh file dir
+top_dir   = gmx_dir + '/top_files' # topology dir
+cfg_dir   = gmx_dir + '/allcoord_files' # configuration dir
+itp_dir   = gmx_dir + '/itp_files' # prm/itp file dir
+mdp_dir   = gmx_dir + '/mdp_files' # mdp file dir
+scr_dir   = os.environ["SCRATCH"]  #  scratch dir
+pack_exec = home_dir + '/tools/packmol/packmol'
+
+if not os.path.isdir(scr_dir):
+    raise RuntimeError("FATAL ERROR: ", scr_dir, " not found")
+
+scr_dir  = scr_dir + '/cile_systems'
+if not os.path.isdir(scr_dir):
+    raise RuntimeError("FATAL ERROR: ", scr_dir, " not found")
+
+#--------Main analysis---------------------------------------------
+for iarr in range(len(res_dil)): # loop in solvents
+
+    os.chdir(main_dir) # Start from main directory
+    
+    dil_name    = res_dil[iarr]
+    resname_arr = [dil_name]
+    resname_arr.extend(res_list)
+
+    # Check directories
+    head_name = res_org_cat.upper() + '_' + res_org_an.upper() \
+        + '_' + dil_name
+    head_dir = scr_dir + '/' + head_name
+    if not os.path.isdir(head_dir):
+        print(f'ERROR: {head_dir} not found')
+        continue
+
+    sysname = res_salt_cat+res_salt_an.upper() + \
+        str(int(n_li_salt_arr[iarr]))+'_' + res_org_cat.upper() + \
+        res_org_an.upper() + ef.convert_number(rat_il_salt) + '_' + \
+        dil_name.upper() + ef.convert_number(rat_dil_salt)
+
+    workdir = head_dir + '/' + sysname
+    if not os.path.isdir(workdir):
+        print(f'ERROR: {workdir} not found')
+        continue
+
+    # Check if tpr exists - then no need to do anything else
+    if glob.glob(workdir+'/traj_npt_main.trr') == []:
+        print(f'ERROR: No traj_npt_main.trr found in {workdir}')
+        continue
+
+    print('Making analysis input files ..')
+    ef.make_anainps(workdir,res_salt_cat,res_org_cat,resname_arr,\
+                    rdfset_tuple,rdfcalc=1,diffcalc=1\
+                    ,autocorrcalc=0)
+    
+    print('Generating shell script files ...')
+    ef.cpy_anash_files(sh_dir,workdir,sh_ana_fyle,res_salt_cat,\
+                       res_salt_an,res_org_cat,res_org_an,\
+                       dil_name,begtime=20000,endtime=30000,\
+                       runall=runall,jname=sysname)
+    
+    # Cleaning up
+    print('Cleaning up directory ..')
+    ef.clean_up(workdir)
+
 
--- src_py/extra_functions.py
+++ src_py/extra_functions.py
@@ -192,7 +192,59 @@ def setup_packmol(inpfnames,molval_arr,workdir,boxl,\
             fpack.write(f'end structure\n\n')
         
 #------------------------------------------------------------------
-    
+def make_anainps(workdir,saltcatname,orgcatname,resnames,\
+                 rdftuple=list(),rdfcalc=1,diffcalc=1,\
+                 autocorrcalc=1):
+    if rdfcalc:
+        if not os.path.isdir(workdir + '/rdfinps'):
+            os.mkdir(workdir + '/rdfinps')
+
+
+    with open(workdir + '/rdfinps/rdfsel_all.txt','w') as frdf:
+        for res in range(len(resnames)-1):
+            frdf.write(f'resname {resnames[res]};\n')
+        frdf.write(f'resname {resnames[-1]}')
+            
+        if rdftuple:
+            for res,typ in rdftuple:
+                frdf.write(f';\nresname {res} and type {typ}')
+
+    rdfsaltcat = '/rdfinps/ref'+saltcatname+'.txt'
+    with open(workdir + rdfsaltcat,'w') as fs:
+            fs.write(f'resname {saltcatname}')
+
+    rdforgcat = '/rdfinps/ref'+orgcatname+'.txt'
+    with open(workdir + rdforgcat,'w') as fs:
+            fs.write(f'resname {orgcatname}')
+#------------------------------------------------------------------
+# Copy and run shell script files
+def cpy_anash_files(srcdir,destdir,sh_ana_fyle,saltcat,\
+                    saltan,orgcat,organ,diluent,begtime,endtime,\
+                    runall=0,jname="default"):
+
+    gencpy(srcdir,destdir,sh_ana_fyle)
+    py_fname = destdir + '/' + sh_ana_fyle
+    rev_fname = py_fname.replace('_pyinp','')
+    fr  = open(py_fname,'r')
+    fw  = open(rev_fname,'w')
+    fid = fr.read().replace("py_jobname","r_"+jname).\
+        replace("py_saltcation",saltcat).\
+        replace("py_saltanion",saltan).\
+        replace("py_orgcation",orgcat).\
+        replace("py_organion",organ).\
+        replace("py_diluent",diluent).\
+        replace("py_begtime",str(begtime)).\
+        replace("py_endtime",str(endtime))
+    fw.write(fid)
+    fr.close(); fw.close()
+
+    if runall:
+        os.chdir(destdir)
+        print(f"Submitting {rev_fname} ..")
+        subprocess.call(["chmod", "777", rev_fname])
+        subprocess.call(["sbatch", rev_fname])
+#------------------------------------------------------------------
+
 # Copy and run shell script files
 def cpy_sh_files(srcdir,destdir,sh_pp_fyle,sh_md_fyle,boxl,\
                  runall=0,outname='mixture.pdb',\
